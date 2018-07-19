4eeecf91408924596b4b0f68d42c7a103ebc5be3
Ciro Stodulski de Azevedo <ciro.azevedo@meta.com.br>
2018-07-19 16:24:47 -0300

    menu responsivo

--- aplicacao/src/app/main/header/header.component.ts
+++ aplicacao/src/app/main/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  toggle: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleMenu() {
+    this.toggle
+      ? this.toggle = false
+      : this.toggle = true;
+  }
 }
