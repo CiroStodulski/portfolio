7a646bb44b7b9c2c82eff9a8706b28f0a9d217e1
ciro stodulski de azevedo <ciro.sda@gmail>
2018-07-20 19:05:36 -0300

    initial deploy

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/app/dist/aplicacao')); //aqui você define onde está o index.html da sua aplicação.
+var port = process.env.PORT || 3000;
+console.log('oi')
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
